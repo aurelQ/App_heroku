d5ceaf8061d335fc52974fb535a025004e9c1ee3
aurelQ <aurelquetin@hotmail.fr>
2022-03-31 04:48:06 +0200

    Dernier commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: sh setup.sh && gunicorn -w 4 -k uvicorn.workers.UvicornWorker main_test:app   
+web: sh setup.sh && gunicorn -w 4 -k uvicorn.workers.UvicornWorker main_api:app   
--- main_api.py
+++ main_api.py
@@ -6,7 +6,7 @@ import pandas as pd
 from pydantic import BaseModel
 import sklearn.metrics
 import pickle
-
+ 
 # FastAPI libray
 from fastapi import FastAPI
 
@@ -14,15 +14,10 @@ from fastapi import FastAPI
 app = FastAPI(title='Placement Analytics', version='0.24.2',
               description='Lightgbm model is used for prediction')
 
-# Initialize model artifacte files. This will be loaded at the start of FastAPI model server.
-#grid_result_xgb_w  = joblib.load('../model/grid_result_xgb_w.joblib')
-#xgb_wclf = joblib.load('model/clf_xgb_w.joblib')
-X_test=pd.read_csv('./model/X_train_ech.csv')
-#explainer=joblib.load('model/explainer_xgb.joblib')
+# Récupération du jeu de données Test
+X_test=pd.read_csv('./model/X_test_ech.csv')
 
-
-
-# read pickle files
+# Lecture du fichier pickle
 with open('./model/score_objects2.pkl', 'rb') as handle:
     clf_xgb_w, explainer_xgb = pickle.load(handle)  
 
@@ -35,18 +30,17 @@ class Data(BaseModel):
 @app.get('/')
 @app.get('/home')
 def read_home():
-    """
-     Home endpoint which can be used to test the availability of the application.
-     """
-    return {'message': 'System is healthy'}
+
+    return {"Information': 'L'API est déployée"}
 
 # ML API endpoint for making prediction aganist the request received from client
 @app.post("/predict")
 def predict(data: Data):
-    # Extract data in correct order
-    data_dict = data.dict() #{data_dict={'test_id':3}} #    data_df = pd.DataFrame.from_dict([data_dict])
+
+    # Création d'un dictionnaire
+    data_dict = data.dict() 
     var=data_dict['test_id']
-    X_test_df=X_test[X_test["Unnamed: 0"]==var]   #  data_df_2=df_csv.loc[var]
+    X_test_df=X_test[X_test["Unnamed: 0"]==var]  
     X_test_df=X_test_df.to_dict('series')
     del X_test_df['Unnamed: 0']
     X_test_df=pd.DataFrame.from_dict([X_test_df])
@@ -67,14 +61,11 @@ def predict(data: Data):
 
     #Création du dictionnaire de données de sortie 
     result_dict={"prediction":prediction, "score":score}
-    #result_dict={"prediction":[], "score":[]}
-    #result_dict["prediction"].append(prediction)
-    #result_dict["score"].append(score)
-    print("result_dict :", result_dict )
-    # Return response back to client
+
+    # Retourne le dictionnaire
     return result_dict
 
 if __name__ == '__main__':
     uvicorn.run("main_test:app", host="0.0.0.0", port=8000, reload=True)
- #  uvicorn.run("main_test:app" reload=True)
+
     
